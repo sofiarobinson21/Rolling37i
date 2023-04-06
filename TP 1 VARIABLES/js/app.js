de31636cb79c2a0358e07f922a48216281a1af77
sofirobinson <sofi_robinson21@hotmail.com>
2023-04-05 21:05:47 -0300

    agrego trabajo practico numero 1. falta resolver ejercicios 9 y 11(consultar)

---
+++ TP 1 VARIABLES/js/app.js
@@ -0,0 +1,63 @@
+// 1.- Escribe un programa de una sola línea que haga que aparezca en la pantalla un alert que diga “un mensaje”.
+// alert (`Tienes un mensaje`)
+
+// 2.- Escribe un programa de una sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+// document.write(`HELLO WORLD!`)
+
+// 3.- Escribe un programa de una sola línea que escriba en la pantalla el resultado de sumar 3 + 5.
+// document.write(suma=3+5)
+
+// 4.- Escribe un programa de dos líneas que pida el nombre del usuario con un prompt y escriba un texto que diga «Hola nombreUsuario»
+// let nombre= prompt(`Ingrese su nombre`);
+// document.write(`hola! ${nombre}`)
+
+// 5.- Escribe un programa de tres líneas que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+//  let numero1= parseInt(prompt(`Ingrese un valor`));
+//  let numero2= parseInt(prompt(`Ingrese otro valor`));
+//  let suma= numero1+numero2
+//  document.write(`El resultado de la suma entre ${numero1} y ${numero2} es ${suma}`)
+
+// 6.-Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+// let numero1= parseInt(prompt(`Ingrese un valor`));
+// let numero2= parseInt(prompt(`Ingrese otro valor`));
+// if(numero1>numero2){
+//     document.write(`el numero mas grande ingresado es ${numero1}`)
+// }else if(numero2>numero1){
+//     document.write(`el numero mas grande ingresado es ${numero2}`)
+// }
+
+// 7.- Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+// let numero1= parseInt(prompt(`Ingrese un valor`));
+// let numero2= parseInt(prompt(`Ingrese otro valor`));
+// let numero3= parseInt(prompt(`ingrese tercer valor`))
+// document.write(`el mayor numero ingresado es  ${Math.max(numero1, numero2, numero3)}`)
+
+// 8.- Escribe un programa que pida un número y diga si es divisible por 2
+// let numero1= parseInt(prompt(`Ingrese un valor`));
+// if(numero1 % 2 == 0){
+//     document.write(`El ${numero1} es divisible por 2`)
+// } else {
+//     document.write(`${numero1} No es divisible por dos`)
+// }
+
+// 9.- Escribe un programa que pida una frase y escriba las vocales que aparecen
+// Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)
+// let string= prompt(`inserte una frase`)
+// string.length(function)
+
+
+// 10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+// let numero1= parseInt(prompt(`Ingrese un valor`));
+// if(numero1 % 2 == 0){
+//      document.write(`El ${numero1} es divisible por 2`)
+// } else if(numero1 % 3 ==0){
+//     document.write(`${numero1} es divisible por tres`)
+// } else if(numero1 % 5 ==0){
+//     document.write(`${numero1} es divisible por cinco`)
+// } else if(numero1 % 7 ==0){
+//     document.write(`${numero1} es divisible por siete`)
+// } else{
+//     document.write(` ${numero1} NO es divisible ni por 2 ni por 3 ni por 5 ni por 7`)
+// }
+
+// 11.- Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
